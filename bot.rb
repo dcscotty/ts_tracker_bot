c67ad2a1e5d385c0238ce3bc10162075bc6763c0
David Scott <david.tech@gmail.com>
2017-10-08 11:45:40 -0700

    Cleaned up directory structure, and added new poll creation class and commands

--- bot.rb
+++ bot.rb
@@ -1,10 +1,9 @@
-require_relative 'mixins/message_processor'
-require_relative 'mixins/report_manager'
-require_relative 'mixins/messenger'
+Dir['./mixins/*.rb'].each {|file| require file }
 require 'dotenv/load'
 require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], client_id: ENV['CLIENT_ID'], prefix: '!'
+messenger = Messenger.new
 
 puts "#{bot.invite_url}"
 
@@ -16,11 +15,26 @@ end
 bot.command(:report, min_args: 2, max_args: 2, channels: [ENV['REPORT_CHANNEL'].to_i], description: 'Returns a report for #spent10 or #followtwo and the number of days in the past specified. Usage: !report #spent10 21') do |event, type, days|
   case type
   when '#spent10'
-    Messenger.report(event: event, report_type: type, days: days)
+    messenger.report(event: event, report_type: type, days: days)
   when '#followtwo'
-    Messenger.report(event: event, report_type: type, days: days)
+    messenger.report(event: event, report_type: type, days: days)
   else
     event.respond "I'm sorry Dave, I can't do that."
+  end
+end
+
+bot.command(:createpoll, description: 'Creates a poll on strawpoll.me. Usage: !createpoll Title, Option A, Option B, Option C') do |event, *args|
+  poll_arguments = args.join(' ').split(', ')
+  @pm = PollManager.new(poll_args: poll_arguments)
+  @pm.create # Creates the poll and sets the attribute poll_link/poll_results on @pm
+  event.respond "Vote here: #{@pm.poll_link}"
+end
+
+bot.command(:poll, description: 'Returns the link to the last created poll') do |event|
+  if @pm
+    event.respond "The last created poll can be found here: #{@pm.poll_link}"
+  else
+    event.respond "A poll must be previously created (or I have gone offline since the last poll was made)."
   end
 end
 
---
+++ mixins/poll_manager.rb
@@ -0,0 +1,45 @@
+require 'rest-client'
+
+class PollManager
+  REQUEST_URL = 'https://strawpoll.me/api/v2/polls'
+  attr_reader :title, :options, :poll_link
+
+  def initialize(poll_args:)
+    @title = poll_args.first
+    @options = poll_args.drop(1)
+  end
+
+  def create
+    poll = create_poll
+    poll_id = JSON.parse(poll.body)["id"]
+    @poll_link = poll_url(id: poll_id)
+  end
+
+  private
+
+  def create_poll
+    begin
+      RestClient.post(REQUEST_URL, request_body.to_json, headers = {content_type: :json})
+    rescue RestClient::ExceptionWithResponse => error
+      case error.http_code
+      when 301, 302, 307
+        error.response.follow_redirection
+      else
+        raise
+      end 
+    end
+  end
+
+  def poll_url(id:)
+    "http://www.strawpoll.me/#{id}"
+  end
+
+  def request_body
+    {
+      "title": title,
+      "options": options,
+      "multi": false
+    }
+  end
+
+end
